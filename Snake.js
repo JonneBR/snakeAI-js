7663d81703455c4301a99fb3493b9e7ece683df8
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2023-09-17 19:56:00 -0300

    feat: snakes body

--- Snake.js
+++ Snake.js
@@ -11,15 +11,27 @@ export default class Snake {
 
     this.food;
 
-    this.head = new Vector(800, settings.height / 2);
     this.food = new Food();
     this.body = new Array();
+
+    this.head = new Vector(800, settings.height / 2);
+    this.body.push(new Vector(800, settings.height / 2 + settings.SIZE)); //new Vector(800, 420)
+    this.body.push(new Vector(800, settings.height / 2 + 2 * settings.SIZE)); //new Vector(800, 440)
   }
 
+  //show the snake
   show() {
+    console.log(this.body);
     this.food.show();
     renderer.fillStyle("rgb(255,255,255)");
     renderer.strokeStyle("rgb(0,0,0)");
+    //loop to show snake's body
+    for (let i = 0; i < this.body.length; i++) {
+      renderer.fillRect(this.body[i].x, this.body[i].y, settings.SIZE, settings.SIZE);
+      renderer.rect(this.body[i].x, this.body[i].y, settings.SIZE, settings.SIZE);
+    }
+    renderer.fillStyle("grey");
     renderer.fillRect(this.head.x, this.head.y, settings.SIZE, settings.SIZE);
+    renderer.rect(this.head.x, this.head.y, settings.SIZE, settings.SIZE);
   }
 }
